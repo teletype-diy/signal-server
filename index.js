6a4387229c04764655e19d08f6f32fa57735a958
Dominik Schachten <schadomi@mail.uni-paderborn.de>
2024-02-10 20:43:41 +0100

    Add drafty signaling server

---
+++ index.js
@@ -0,0 +1,48 @@
+const { Server } = require("socket.io");
+
+const io = new Server({ /* options */ });
+
+const connected_rooms = []
+
+io.on("connection", async (socket) => {
+
+    console.log(socket.id);
+
+
+    // ...
+    // const userId = await computeUserIdFromHeaders(socket);
+    const userId = "dummy"
+
+    socket.join(userId);
+
+    // and then later
+    // io.to(userId).emit("peer_info", connected_rooms);
+
+    socket.on("halo_subscribe", (roomName) => {
+        console.log(`new halo_subscribe for ${roomName}`);
+        if (!connected_rooms.includes(roomName)) {
+            connected_rooms.push(roomName)
+        }
+        socket.join(roomName);
+    });
+
+    socket.on("halo", (roomName, eventName, data) => {
+        console.log(`new msg for room: ${roomName}`);
+        if (!connected_rooms.includes(roomName)) {
+            console.log("is a new room...");
+            connected_rooms.push(roomName);
+            // socket.on(roomName, (arg, arg1) => {
+            //     // do something clever
+            // })
+        }
+        socket.join(roomName);
+        console.log(data);
+        io.to(roomName).emit("peer_info", eventName, data);
+        // connected_rooms.push(arg)
+        // io.to(userId).emit("peer_info", connected_client_list);
+    })
+
+
+});
+
+io.listen(3456);
