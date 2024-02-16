1fe98ffd2dae2a97affdb3eb08aa8a548af3c846
Dominik Schachten <schadomi@mail.uni-paderborn.de>
2024-02-16 23:38:08 +0100

    Add dockerfile and docker compose, enable server to use postgres optionally

--- index.js
+++ index.js
@@ -2,21 +2,39 @@ const { Server } = require("socket.io");
 
 const io = new Server({ /* options */ });
 
-const connected_rooms = []
+// conditonal import of postgres
+// npm install @socket.io/postgres-adapter pg
+try {
+    const { createAdapter } = require("@socket.io/postgres-adapter");
+    const { Pool } = require("pg");
+
+    const pool = new Pool({
+      user: process.env.POSTGRES_USER || "postgres",
+      host: process.env.DBHOST || "postgres",
+      database: process.env.POSTGRES_DB || "postgres",
+      password: process.env.POSTGRES_PASSWORD || "secret",
+      port: 5432,
+    });
+
+    pool.query(`
+      CREATE TABLE IF NOT EXISTS socket_io_attachments (
+          id          bigserial UNIQUE,
+          created_at  timestamptz DEFAULT NOW(),
+          payload     bytea
+      );
+    `);
+
+    io.adapter(createAdapter(pool));
+    console.log("Using postgres adapter");
+} catch (e) {
+    console.log("Not using postgres adapter");
+    console.log(e);
+}
+// conditonal import of postgres - end
+
 
 io.on("connection", async (socket) => {
-
-    console.log(socket.id);
-
-
-    // ...
-    // const userId = await computeUserIdFromHeaders(socket);
-    const userId = "dummy"
-
-    socket.join(userId);
-
-    // and then later
-    // io.to(userId).emit("peer_info", connected_rooms);
+    // console.log(socket.id);
 
     socket.on("protocol_version", (callback) => {
         // socket.emit("protocol_version", 9);
@@ -24,27 +42,16 @@ io.on("connection", async (socket) => {
         callback({ok: true, body: {version: 9}});
     })
 
+    // connect peers into their channels
     socket.on("halo_subscribe", (roomName) => {
         console.log(`new halo_subscribe for ${roomName}`);
-        if (!connected_rooms.includes(roomName)) {
-            connected_rooms.push(roomName)
-        }
         socket.join(roomName);
     });
 
+    // send a new msg to a channel
     socket.on("halo", (roomName, eventName, data) => {
         console.log(`new msg for room: ${roomName}`);
-        if (!connected_rooms.includes(roomName)) {
-            console.log("is a new room...");
-            connected_rooms.push(roomName);
-            // socket.on(roomName, (arg, arg1) => {
-            //     // do something clever
-            // })
-        }
-        // TODO: this is almost certainly wrong. but it only works if it is here... what does that mean.
-        // nvm still appears to work.. go to sleep----
-        // socket.join(roomName);
-        console.log(data);
+        // console.log(data);
         io.to(roomName).emit("peer_info", eventName, data);
         // connected_rooms.push(arg)
         // io.to(userId).emit("peer_info", connected_client_list);
